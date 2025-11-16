1a486bb6cd1bb309c56e15d61e38be2f0b04639f
Jhon849 <jhon.moncayo@campusucc.edu.co>
2025-11-16 11:00:34 -0500

    Initial Spring Boot project setup for Streamora Backend

---
+++ src/test/java/com/streamora/backend/StreamoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
