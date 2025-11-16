4c1538dbed52b3f3524cd93465b513f172d1f655
Jhon849 <jhon.moncayo@campusucc.edu.co>
2025-11-16 16:33:57 -0500

    Arreglar CloudinaryConfig

--- src/main/java/com/streamora/backend/cloudinary/CloudinaryConfig.java
+++ src/main/java/com/streamora/backend/cloudinary/CloudinaryConfig.java
@@ -2,20 +2,20 @@ package com.streamora.backend.cloudinary;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud-name}")
+    @Value("${cloudinary.cloud_name}")
     private String cloudName;
 
-    @Value("${cloudinary.api-key}")
+    @Value("${cloudinary.api_key}")
     private String apiKey;
 
-    @Value("${cloudinary.api-secret}")
+    @Value("${cloudinary.api_secret}")
     private String apiSecret;
 
     @Bean
@@ -27,3 +27,6 @@ public class CloudinaryConfig {
         ));
     }
 }
+
+
+
